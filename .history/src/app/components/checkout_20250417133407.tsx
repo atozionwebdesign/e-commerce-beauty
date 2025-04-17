2660a724d592ea375f4f294c7234bca6eeb9ac17
A To Zion Web Design, LLC -By Full Stack Developer Ashley Stith <ashley.stith@atozionwebdesign.com>
2025-04-17 13:36:15 -0400

    updating checkout api headers

--- .history/src/app/components/checkout_20250417133407.tsx
+++ .history/src/app/components/checkout_20250417133407.tsx
@@ -21,7 +21,8 @@ async function fetchClientSecret(){
       headers : { 
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          "Access-Control-Allow-Origin" : "*", "Access-Control-Allow-Credentials" : 'true' ,
+          "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : 'true' ,
       }, 
       body: JSON.stringify(data),
   });
